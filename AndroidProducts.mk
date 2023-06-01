30878402ba48074683fdfe6f97cb66a9e79f8224
NotZeetaa <67799176+NotZeetaa@users.noreply.github.com>
2023-06-01 10:25:08 +0100

    alioth: Adapt to Yaap [2/2]

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_alioth.mk
+    $(LOCAL_DIR)/yaap_alioth.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_alioth-user \
-    lineage_alioth-userdebug \
-    lineage_alioth-eng
+    yaap_alioth-user \
+    yaap_alioth-userdebug \
+    yaap_alioth-eng
